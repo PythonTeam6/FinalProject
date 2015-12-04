42e3443f398dc23f3ca55f5610b26235b0c3e0ed
hipoa <lockband@naver.com>
2015-12-04 20:26:13 +0900

    QT 다이얼로그2
    resizeEvent 수정, 인스턴스 수정

--- TeamQT/TeamQT/TeamQT.py
+++ TeamQT/TeamQT/TeamQT.py
@@ -9,50 +9,65 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
 
 
-class Form(QtWidgets.QDialog):
+class Form(QtWidgets.QMainWindow):
     
-    #def resizeEvent(self ,resizeEvent):
-    #    QtWidgets.QMessageBox.information(self,"Information!","Window has been resized...")    
+    def resizeEvent(self ,resizeEvent):
+        geo = self.frameGeometry()   
+        print(geo.width(), geo.height())
+        
+        width = geo.width()
+        height = geo.height()
+
+        self.treeWidget.resize(width, int(height*521/631))
+        self.treeWidget.setColumnWidth(0,int(width*2/11))
+        self.treeWidget.setColumnWidth(1,int(width*5/11))
+        self.treeWidget.setColumnWidth(2,int(width*2/11))
+        self.treeWidget.setColumnWidth(3,int(width*2/11))
+        #QtWidgets.QMessageBox.information(self,"Information!","Window has been resized...")    
     
 
     def __init__(self, parent=None):
-        QtWidgets.QDialog.__init__(self, parent)
+        QtWidgets.QMainWindow.__init__(self)
         
-        self.ui = uic.loadUi("qt3.ui")        
+        #self.ui = uic.loadUi("qt3.ui")        
+        uic.loadUi("qt3.ui", self)
         
-        width = 1000
-        height = 600
+        geo = self.frameGeometry()        
+        print(geo.width(), geo.height())
+        width = geo.width()
+        height = geo.height()
         
-        self.ui.treeWidget.setColumnWidth(0,int(width*2/11))
-        self.ui.treeWidget.setColumnWidth(1,int(width*5/11))
-        self.ui.treeWidget.setColumnWidth(2,int(width*2/11))
-        self.ui.treeWidget.setColumnWidth(3,int(width*2/11))
+
+        self.treeWidget.setColumnWidth(0,int(width*2/11))
+        self.treeWidget.setColumnWidth(1,int(width*5/11))
+        self.treeWidget.setColumnWidth(2,int(width*2/11))
+        self.treeWidget.setColumnWidth(3,int(width*2/11))
                
-        self.ui.actionAdd_Files.setShortcut('Ctrl+O')
-        self.ui.actionAdd_Files.setStatusTip('Open new File')
-        self.ui.actionAdd_Files.triggered.connect(self.addButton)
+        self.actionAdd_Files.setShortcut('Ctrl+O')
+        self.actionAdd_Files.setStatusTip('Open new File')
+        self.actionAdd_Files.triggered.connect(self.OnClickAdd)
         
-        self.ui.convertButton.pressed.connect(self.convertButton)
-        self.ui.addButton.pressed.connect(self.addButton)
-        self.ui.formatButton.pressed.connect(self.formatButton)
-        self.ui.pathButton.pressed.connect(self.pathButton)
-        self.ui.show()
+        self.convertButton.pressed.connect(self.OnClickConvert)
+        self.addButton.pressed.connect(self.OnClickAdd)
+        self.formatButton.pressed.connect(self.OnClickFormat)
+        self.pathButton.pressed.connect(self.OnClickPath)
+        #self.show()
 
     
-    def convertButton(self):
+    def OnClickConvert(self):
         print("I'm convert Button")
-    def formatButton(self):
+    def OnClickFormat(self):
         print("I'm format Button")
-    def pathButton(self):
+    def OnClickPath(self):
         print("I'm path Button")
     
-    def addButton(self):
+    def OnClickAdd(self):
         fnames = QtWidgets.QFileDialog.getOpenFileNames(self, 'Open file', '/home')
         if len(fnames[0]) == 0:
             return
         else:
             for k in range(len(fnames)):
-                a = QtWidgets.QTreeWidgetItem(self.ui.treeWidget)
+                a = QtWidgets.QTreeWidgetItem(self.treeWidget)
                 a.setText(0, "unchanged")
                 a.setText(1, fnames[0][k])
                 a.setText(2, os.path.basename(fnames[0][k]))
@@ -70,4 +85,5 @@ class Form(QtWidgets.QDialog):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     myForm = Form()
+    myForm.show()
     sys.exit(app.exec())
