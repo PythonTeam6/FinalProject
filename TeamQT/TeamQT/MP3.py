f73eac640bbbe0b76e0d0a481fafa34a907674d8
kbj627 <gbj627@gmail.com>
2015-12-06 15:58:22 +0900

    mergy project
    merge

--- TeamQT/TeamQT/MP3.py
+++ TeamQT/TeamQT/MP3.py
@@ -1,17 +1,10 @@
-# coding: utf-8
- 
-import sys
+﻿import sys
 import os
 from PyQt5 import QtWidgets, QtCore
 from PyQt5 import QtGui
 from PyQt5 import uic
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
-
-from DB_Control import *
-from Web_Parser import *
-from MP3 import *
-from Video import *
 
 
 class Form(QtWidgets.QMainWindow):
@@ -35,7 +28,7 @@ class Form(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         
         #self.ui = uic.loadUi("qt3.ui")        
-        uic.loadUi("qt3.ui", self)
+        uic.loadUi("qtMp3.ui", self)
         
         geo = self.frameGeometry()        
         print(geo.width(), geo.height())
@@ -81,45 +74,3 @@ class Form(QtWidgets.QMainWindow):
     @pyqtSlot()
     def convertSlot(self):
         print('click')
-
-
-def melonParsing():
-    dbc = DBControl("test.db")
-    artistDB = DBControl("artist.db")
-
-    l = list(artistDB.getDataList())
-    print(l)
-
-    jparse = JSON_Parser()
-    l = list(dbc.getDataList())
-    
-    #str = ('You Kyung',)
-    #l = l[l.index(str):]
-
-    #dbc.init_DB()
-    #for item in l:
-    #    result = artistDB.findArtist(item[0]).fetchall()
-    #    if len(result) == 0:
-    #        artists = jparse.request_Melon(item[0])
-    #        for artist in artists:
-    #            result = artistDB.findArtist(artist).fetchall()
-    #            if len(result) == 0:
-    #                print(artist)
-    #                artistDB.insertData(artist)
-    #    else:
-    #        print('Overlap\n')
-
-if __name__ == '__main__':
-    #melonParsing()
-
-    #Video
-    app = QtWidgets.QApplication(sys.argv)
-    myWindow = Form()
-    myWindow.show()
-    sys.exit(app.exec())
-    
-    #mp3
-    #app = QtWidgets.QApplication(sys.argv)
-    #myForm = Form()
-    #myForm.show()
-    #sys.exit(app.exec())
